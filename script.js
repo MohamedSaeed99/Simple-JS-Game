f65d69dd7d50e937d200fbdafbf1dec3fd93d293
MohamedSaeed99 <msaeed7@uic.edu>
2019-11-13 17:32:36 -0600

    changed characters to fit theme

--- script.js
+++ script.js
@@ -6,39 +6,42 @@ let player = {
 
 let gameStat = {
     score: 0,
-    lives: [document.getElementById("heart")]
+    lives: [document.getElementById("heart"), document.getElementById("heart"), document.getElementById("heart")]
 };
 
 let level = 0;
 
+let left = () => {
+    player.x -=20;
+}
 let mobileMovement = () => {
-    $(".left").on('click', function(){
-        player.x -=7;
+    $(".left").on('mousedown', function(){
+        player.x -=20;
     });
     $(".up").on('click', function(){
-        player.y -=7;
+        player.y -=20;
     });
     $(".right").on('click', function(){
-        player.x +=7;
+        player.x +=20;
     });
     $(".down").on('click', function(){
-        player.y +=7;
+        player.y +=20;
     });
 }
 //check for holding down the arrow button
 let movement =() =>{
     document.addEventListener('keydown', function(logKey){
         if(logKey.keyCode == 38){
-            player.y-=7;
+            player.y-=20;
         }
         else if(logKey.keyCode == 40){
-            player.y+=7;
+            player.y+=20;
         }
         else if(logKey.keyCode == 39){
-            player.x+=7;
+            player.x+=20;
         }
         else if(logKey.keyCode == 37){
-            player.x-=7;
+            player.x-=20;
         }
     });
 }
@@ -58,11 +61,11 @@ let randomWidth = (maxWidth, items, current) =>{
 }
 
 let randomHeight = () => {
-    var randVal = Math.floor(Math.random() * Math.floor(-50) + -15)
+    var randVal = Math.floor(Math.random() * Math.floor(-100) + -70)
     return randVal;
 }
 let randomSpeed = (val) => {
-    var randVal = Math.floor(Math.random() * Math.floor(val)+1)
+    var randVal = Math.floor(Math.random() * Math.floor(val)+2)
     return randVal;
 }
 
@@ -91,7 +94,31 @@ let items = [{
     y: randomHeight(),
     speed: randomSpeed(level),
     image: document.getElementById("harm")
-}, {
+},{
+    good: false,
+    x: 0,
+    y: randomHeight(),
+    speed: randomSpeed(level),
+    image: document.getElementById("harm")
+},{
+    good: false,
+    x: 0,
+    y: randomHeight(),
+    speed: randomSpeed(level),
+    image: document.getElementById("harm")
+},{
+    good: false,
+    x: 0,
+    y: randomHeight(),
+    speed: randomSpeed(level),
+    image: document.getElementById("harm")
+},{
+    good: true,
+    x: 0,
+    y: randomHeight(),
+    speed: randomSpeed(level),
+    image: document.getElementById("good")
+},{
     good: true,
     x: 0,
     y: randomHeight(),
@@ -116,8 +143,8 @@ let levelComplete = (items)=>{
 }
 
 let collision = (items, current) =>{
-    if(player.x < items[current].x + 25 && 25 + player.x > items[current].x
-        && player.y < items[current].y + 15 && 15 + player.y > items[current].y){
+    if(player.x < items[current].x + 40 && 15 + player.x > items[current].x
+        && player.y < items[current].y + 40 && 15 + player.y > items[current].y){
         if(items[current].good){
             items.splice(current, 1);
             gameStat.score += 100;
@@ -141,16 +168,13 @@ let collision = (items, current) =>{
 window.onload = function() {
     var c = document.getElementById("canvas");
     var ctx = c.getContext("2d");
-    ctx.font = "10px Arial";
-    ctx.drawImage(player.image, c.width/2, c.height-20, 25, 15);
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+
+    ctx.font = "30px Arial";
+
     player.x = c.width/2;
-    player.y = c.height-20;
-    var j = 1;
-    this.console.log(gameStat.lives[0])
-    for(var i = 0; i < gameStat.lives.length; i++){
-        ctx.drawImage(gameStat.lives[i], j*8, 5, 10, 5);
-        j++;
-    }
+    player.y = c.height-50;
 
     //call this once
     movement();
@@ -160,12 +184,14 @@ window.onload = function() {
     var game = () => {
         ctx.clearRect(0, 0, c.width, c.height);
 
-        ctx.fillText('Score: ' + gameStat.score, c.width-60, 10);
-        ctx.fillText('Level: ' + level, c.width-60, 20);
+        //fillBackground(ctx);
+
+        ctx.fillText('Score: ' + gameStat.score, c.width-200, 30);
+        ctx.fillText('Level: ' + level, c.width-200, 60);
 
         //sets game boundaries for player
-        if(player.x > c.width-25){
-            player.x = c.width-25;
+        if(player.x > c.width-50){
+            player.x = c.width-50;
         }
         else if(player.x < 0){
             player.x = 0
@@ -173,31 +199,36 @@ window.onload = function() {
         if(player.y < 0){
             player.y = 0;
         }
-        else if(player.y > c.height-15){
-            player.y = c.height-15;
+        else if(player.y > c.height-50){
+            player.y = c.height-50;
         }
         var j = 1
         for(var i = 0; i < gameStat.lives.length; i++){
-            ctx.drawImage(gameStat.lives[i], j*8, 5, 10, 5);
+            ctx.drawImage(gameStat.lives[i], j*30, 5, 30, 30);
             j++;
         }
 
         //draws updated items
         for(var i = 0; i < items.length; i++){
+
             //sets boundaries of items
             if(items[i].y >= (c.height-15)){
                 items[i].y = randomHeight();
             }
             
-            if(items[i].y < -15){
-                items[i].x = randomWidth(c.width-25, items, i);
+            if(items[i].y < -70){
+                items[i].x = randomWidth(c.width-40, items, i);
                 items[i].speed = randomSpeed(level)
             }
             //moves the items
             items[i].y += items[i].speed
 
             if(!collision(items, i)){
-                ctx.drawImage(items[i].image, items[i].x, items[i].y, 25, 15);
+                if(items[i].good){
+                    ctx.drawImage(items[i].image, items[i].x, items[i].y, 100, 100);
+                }
+                else
+                    ctx.drawImage(items[i].image, items[i].x, items[i].y, 40, 40);
             }
         }
 
@@ -219,12 +250,13 @@ window.onload = function() {
             }
         }
 
-        ctx.drawImage(player.image, player.x, player.y, 25, 15);
+        ctx.drawImage(player.image, player.x, player.y, 50, 50);
 
         //checks end game
         if(gameStat.lives.length == 0){
-            ctx.font = "40px Arial";
-            ctx.fillText('GAME OVER', 30, c.height/2);
+            ctx.textBaseline = "top"
+            ctx.font = "70px Arial";
+            ctx.fillText('GAME OVER', c.width/2-200, c.height/2-20);
         }
         else{
             window.requestAnimationFrame(game);
